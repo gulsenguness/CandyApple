0edd8177ed4f8c8438ea2c16547fd1e306f23cb6
Gülşen Güneş <gulsenurgunes@gmail.com>
2024-08-30 14:32:44 +0300

    edited

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,4 +51,8 @@ dependencies {
 
     implementation(libs.androidx.navigation.fragment)
     implementation(libs.androidx.navigation.ui)
+
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
 }
